3f847ef7380630f642e197941371240dd9093d6f
MDi <calendarzyk@gmail.com>
2021-07-10 16:47:49 +0200

    tasks module 23.1 updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    const apiRoot = 'http://sleepy-temple-38669.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+   const trelloApiRoot = 'http://sleepy-temple-38669.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
